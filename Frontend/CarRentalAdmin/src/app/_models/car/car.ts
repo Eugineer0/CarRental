fc6793f553ce1372b115dc9a061da88e7a5dd229
Eugineer0 <evgenbp1@gmail.com>
2022-04-05 17:45:08 +0300

    Separated complex components

--- Frontend/CarRentalAdmin/src/app/_models/car/car.ts
+++ Frontend/CarRentalAdmin/src/app/_models/car/car.ts
@@ -1,23 +1,23 @@
-import { CarService } from "./car-service";
+import { CarService } from './car-service';
 
 export interface Car {
-  registrationNumber: string;
-  brand: string;
-  model: string;
-  seatPlaces: number;
-  averageConsumption: number;
-  gearboxType: GearBoxTypes;
-  weight: number;
-  length: number;
-  power: number;
-  pricePerMinute: number;
-  pricePerHour: number;
-  pricePerDay: number;
-  availableServices: CarService[];
+    registrationNumber: string;
+    brand: string;
+    model: string;
+    seatPlaces: number;
+    averageConsumption: number;
+    gearboxType: GearBoxTypes;
+    weight: number;
+    length: number;
+    power: number;
+    pricePerMinute: number;
+    pricePerHour: number;
+    pricePerDay: number;
+    availableServices: CarService[];
 }
 
 export enum GearBoxTypes {
-  Mechanical,
-  Automatic,
-  Robotic
+    Mechanical,
+    Automatic,
+    Robotic
 }
--- Frontend/CarRentalAdmin/src/app/users/users.component.ts
+++ Frontend/CarRentalAdmin/src/app/users/users.component.ts
@@ -1,39 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
-import { UserService } from "../_services/user.service";
-import { User } from "../_models/user/user";
+import { UserService } from '../_services/user.service';
+import { User } from '../_models/user/user';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  public users: User[] = [];
+    public users: User[] = [];
 
-  constructor(
-    private userService: UserService,
-  ) {
-  }
+    constructor(
+        private userService: UserService
+    ) {
+    }
 
-  public ngOnInit(): void {
-    this.getUsers()
-  }
+    public ngOnInit(): void {
+        this.getUsers();
+    }
 
-  public showDateOfBirth(user: User): string {
-    return new Date(user.dateOfBirth).toDateString().slice(4);
-  }
+    public approveClient(user: User): void {
+        this.userService.approveClient(user.username)
+            .subscribe(
+                _ => {
+                    user.approvalRequested = false;
+                }
+            );
+    }
 
-  private getUsers(): void {
-    this.userService.getUsers()
-      .subscribe(
-        users => {
-          this.users = users;
-        }
-      )
-  }
-
-  approveClient(username: string) {
-    this.userService.approveClient(username).subscribe();
-  }
+    private getUsers(): void {
+        this.userService.getUsers()
+            .subscribe(
+                users => {
+                    this.users = users;
+                }
+            );
+    }
 }
