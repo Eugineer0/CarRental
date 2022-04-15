6d61a17ff216e1ae0e1fa50fde0fbadf5fa32f0e
Eugineer0 <evgenbp1@gmail.com>
2022-04-15 13:27:20 +0300

    Refactored age validator

--- Frontend/CarRentalAdmin/src/app/validators/min-age-validator.ts
+++ Frontend/CarRentalAdmin/src/app/validators/min-age-validator.ts
@@ -1,10 +1,13 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import * as moment from 'moment';
 
 export function minAgeValidator(minAgeYears: number): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-        const criticalDate = moment(control.value).add(minAgeYears, 'year');
-        const isValid = moment().isAfter(criticalDate);
+        const dateOfBirth = new Date(control.value);
+        const criticalDate = new Date(dateOfBirth.setFullYear(dateOfBirth.getFullYear() + minAgeYears));
+        const now = new Date();
+
+        const isValid = criticalDate.getTime() < now.getTime();
+
         return isValid ? null : { minAge: { value: control.value } };
     };
 }
